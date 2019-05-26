de0bd4d46a92388b8e5cee082f97ea67d1950c2f
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-05-26 17:36:45 +0300

    reinitialized as flutter app

---
+++ android/app/src/main/kotlin/sadogan/com/dnotes/MainActivity.kt
@@ -0,0 +1,13 @@
+package sadogan.com.dnotes
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
--- app/src/main/java/com/sadogan/dnotes/NoteData.kt
+++
@@ -1,8 +0,0 @@
-package com.sadogan.dnotes
-
-class NoteData {
-
-
-
-
-}
