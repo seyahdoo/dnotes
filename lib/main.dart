d190cc242b2e39e5bb21e0a3e09547aad93f517e
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-05-26 18:17:32 +0300

    implemented flutter notetaking tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,28 @@
-// Flutter code sample for material.AppBar.actions.1
-
-// This sample shows adding an action to an [AppBar] that opens a shopping cart.
-
 import 'package:flutter/material.dart';
+import 'ViewControllers/HomePage.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: MyStatelessWidget(),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ready, Set, Shop!'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            tooltip: 'Open shopping cart',
-            onPressed: () {
-              // Implement navigation to shopping cart page here...
-              print('Shopping cart opened.');
-            },
-          ),
-        ],
+      debugShowCheckedModeBanner: false,
+      title: 'DNotes',
+      theme: ThemeData(
+        fontFamily: "Roboto",
+        iconTheme: IconThemeData(color: Colors.black),
+        primaryTextTheme: TextTheme(
+          title: TextStyle(color: Colors.black),
+        ),
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
+
     );
   }
 }
